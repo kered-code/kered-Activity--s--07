dc5d19f9549ac4ee136b37691ea50f7eb65f38e7
kered-code <146782844+kered-code@users.noreply.github.com>
2023-10-23 16:04:41 -0600

    Update bot-discord.py

--- bot-discord.py
+++ bot-discord.py
@@ -9,6 +9,18 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
+    
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
 
 @bot.command()
 async def hello(ctx):

f2489875f4d129d1cb094be620953d75c65cabbf
kered-code <146782844+kered-code@users.noreply.github.com>
2023-11-07 22:44:18 -0600

    Create memebot.py

---
+++ memebot.py
@@ -0,0 +1,22 @@
+import discord
+import requests
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+
+async def mem(ctx):
+    with open('images/mem1.jpg', 'rb') as f:
+        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en esta variable!
+        picture = discord.File(f)
+    # A continuación, podemos enviar este archivo como parámetro.
+    await ctx.send(file=picture)
+
+    def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']

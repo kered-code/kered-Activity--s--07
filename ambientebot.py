419dc4fcd06b35bdec153376828630248fee7fbb
kered-code <146782844+kered-code@users.noreply.github.com>
2023-11-13 15:54:47 -0600

    ambientebot.py

--- ambientebot.py
+++ ambientebot.py
@@ -18,4 +18,12 @@ async def sugerencia1(ctx):
 async def sugerencia2(ctx):
     await ctx.send(f'el vidrio tarda en desintegrarse mas de mil años! no lo tires')
 
+@bot.command()
+async def sugerencia3(ctx):
+    await ctx.send(f'aqui te dejo un video que te ayudara: https://youtu.be/nvUqnpicSd0?feature=shared ')
+
+@bot.command()
+async def sugerencia1(ctx):
+    await ctx.send(f'¿porque hay que cuidar el ambiente?: https://youtu.be/aWgHk8Mso0s?feature=shared')
+
 bot.run("")
